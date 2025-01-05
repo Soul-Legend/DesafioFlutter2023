bef8275d52c14a47b1321666c370b594ebdb2ee5
Pedro Taglialenha <124897674+Soul-Legend@users.noreply.github.com>
2025-01-05 15:44:26 -0300

    Add files via upload

---
+++ appv1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import fast_rsa
+import flutter_blue
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FastRsaPlugin.register(with: registry.registrar(forPlugin: "FastRsaPlugin"))
+  FlutterBluePlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlugin"))
+}
